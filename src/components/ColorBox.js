b2264c097d39f1cddebcdaa6f69265fbbdf0b04f
Matt B <mattjballard81@gmail.com>
2020-09-25 11:58:31 -0400

    add hex-code copying functionality and copy overlay animation

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,16 +1,36 @@
 import React, { Component } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./ColorBox.css";
 
 class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { copied: false };
+    this.changeCopyState = this.changeCopyState.bind(this);
+  }
+  // change state to animate copied color, reset after 1.5s
+  changeCopyState() {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => this.setState({ copied: false }), 1500);
+    });
+  }
   render() {
     const { background, name } = this.props;
+    const { copied } = this.state;
     return (
       <div className="ColorBox" style={{ background }}>
+        <div className={`copy-overlay ${copied && "show"}`} style={{ background }} />
+        <div className={`copy-msg ${copied && "show"}`}>
+          <h1>copied!</h1>
+          <p>{background}</p>
+        </div>
         <div className="copy-container">
           <div className="box-content">
             <span>{name}</span>
           </div>
-          <button className="copy-button">Copy</button>
+          <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+            <button className="copy-button">Copy</button>
+          </CopyToClipboard>
         </div>
         <span className="see-more">More</span>
       </div>
