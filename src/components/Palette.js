5aa27c56320ba29f52b7ddd871323dbb125daad3
Matt B <mattjballard81@gmail.com>
2020-09-25 17:38:17 -0400

    add color slider and Navbar

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
+import Navbar from "./Navbar";
 import "./Palette.css";
 
 class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { level: 500 };
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+  changeLevel(level) {
+    this.setState({ level });
+  }
   render() {
-    const colorBoxes = this.props.colors.map(c => <ColorBox background={c.color} name={c.name} />);
+    const { colors } = this.props.palette;
+    const { level } = this.state;
+
+    const colorBoxes = colors[level].map(c => <ColorBox background={c.hex} name={c.name} key={c.name} />);
     return (
       <div className="Palette">
-        {/* Navbar goes here */}
+        <Navbar level={level} changeLevel={this.changeLevel} />
         <div className="Palette-colors">{colorBoxes}</div>
         {/* footer goes here */}
       </div>
