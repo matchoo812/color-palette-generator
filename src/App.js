e91f4fd107d7793ed00de058698ecb2bcba86488
Matt B <mattjballard81@gmail.com>
2020-09-25 16:36:39 -0400

    add color variants to create range of colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import Palette from "./components/Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./ColorVariants";
 
 class App extends Component {
   render() {
+    console.log(generatePalette(seedColors[3]));
     return (
       <div className="App">
         <Palette {...seedColors[3]} />
