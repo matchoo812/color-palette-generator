97b8fd0f0cc3f7732d8efbfe713400bbf4b1bbc6
Matt B <mattjballard81@gmail.com>
2020-09-30 14:09:40 -0400

    Add link to individual Palette from PaletteList

--- src/components/MiniPalette.js
+++ src/components/MiniPalette.js
@@ -49,7 +49,7 @@ function MiniPalette(props) {
   const miniColorBoxes = colors.map(color => <div className={classes.miniColor} style={{ backgroundColor: color.color }} key={color.name}></div>);
 
   return (
-    <div className={classes.root}>
+    <div className={classes.root} onClick={props.handleClick}>
       <div className={classes.colors}>{miniColorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName} <span className={classes.emoji}>{emoji}</span>
